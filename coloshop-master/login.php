f5be7c881cba2ebe3de7ddc676b7a54edb233a9e
QuengLinh <linhthotdai@gmail.com>
2022-06-14 11:57:23 +0700

    @CongTuAoDen2k5

--- coloshop-master/login.php
+++ coloshop-master/login.php
@@ -3,11 +3,21 @@
     $uname = $_POST['email'];
     $upass = $_POST['password'];
 
-    $sql = "Select email, password From user Where email = '$uname' AND password = '$upass'";
-    $rs = mysqli_query($con,$sql);
-    while($r = $rs = mysqli_fetch_assoc($rs)){
+    $sql1 = "Select email, password From user Where email = '$uname' AND password = '$upass' AND id_role = '2'";
+    $rs1 = mysqli_query($con,$sql1);
+    while($r = mysqli_fetch_assoc($rs1)){
         if($r['email'] == $uname && $r['password'] == $upass){
             header("Location: index.php");
+        }else{
+
+        }
+    }
+
+    $sql2 = "Select email, password From user Where email = '$uname' AND password = '$upass' AND id_role = '1'";
+    $rs2 = mysqli_query($con,$sql2);
+    while($r2 = mysqli_fetch_assoc($rs2)){
+        if($r2['email'] == $uname && $r2['password'] == $upass){
+            header("Location: ");
         }else{
 
         }
