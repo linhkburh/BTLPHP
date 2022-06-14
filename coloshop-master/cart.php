51fecdbe6d057dfc611c1a7af89012e338a85cfc
CongTuAoDen2k5 <CongTuAoDen2k5@users.noreply.github.com>
2022-06-14 14:06:28 +0700

    Update cart.php

--- coloshop-master/cart.php
+++ coloshop-master/cart.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>địt mẹ thằng cá </title>
+    <title>Giỏ hàng CONCASHOP</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Colo Shop Template">
