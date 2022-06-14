75ffdf4b10163db63bb3b0ad9151413cca86fce1
CongTuAoDen2k5 <CongTuAoDen2k5@users.noreply.github.com>
2022-06-14 20:01:30 +0700

    Create cart_GiamSLSP.php

---
+++ coloshop-master/cart_GiamSLSP.php
@@ -0,0 +1,8 @@
+<?php
+	include_once('ketnoi.php');
+	$id_pr = $_GET['id_pr'];
+    $sql = "UPDATE order_details set num = num - 1 where id_product = $id_pr";
+    mysqli_query($con,$sql);
+    mysqli_close($con);
+	header("Location: cart.php");
+?>
