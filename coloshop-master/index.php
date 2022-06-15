cc0d915c922fbf440a12efa342de43d65ba6287d
CongTuAoDen2k5 <CongTuAoDen2k5@users.noreply.github.com>
2022-06-15 17:33:54 +0700

    Update index.php

--- coloshop-master/index.php
+++ coloshop-master/index.php
@@ -93,7 +93,7 @@
 							<ul class="navbar_user">
 								<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
 								<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></li>
-								<li class="checkout">
+								<li class="checkout">	
 									<a href="cart.php?key_cart=<?=$temp?>">
 										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
 										<span id="checkout_items" class="checkout_items">2</span>
