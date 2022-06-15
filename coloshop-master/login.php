27660964ddec9df4c1a99d34baee3f0b0610e078
QuengLinh <linhthotdai@gmail.com>
2022-06-15 08:30:59 +0700

    .

--- coloshop-master/login.php
+++ coloshop-master/login.php
@@ -1,26 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+
 <?php
     include_once "ketnoi.php";
-    $uname = $_POST['email'];
+    $uname = $_POST['email1'];
     $upass = $_POST['password'];
-
-    $sql1 = "Select email, password From user Where email = '$uname' AND password = '$upass' AND id_role = '2'";
-    $rs1 = mysqli_query($con,$sql1);
-    while($r = mysqli_fetch_assoc($rs1)){
-        if($r['email'] == $uname && $r['password'] == $upass){
+    $sql = "Select email, password, id_role From user Where email = '$uname' AND password = '$upass'";
+    $rs = mysqli_query($con,$sql);
+    while($r = mysqli_fetch_assoc($rs)){
+        if($r['id_role'] == '2'){
             header("Location: index.php");
-        }else{
-
-        }
-    }
-
-    $sql2 = "Select email, password From user Where email = '$uname' AND password = '$upass' AND id_role = '1'";
-    $rs2 = mysqli_query($con,$sql2);
-    while($r2 = mysqli_fetch_assoc($rs2)){
-        if($r2['email'] == $uname && $r2['password'] == $upass){
-            header("Location: ");
-        }else{
-
+        }else if($r['id_role'] == '1'){
+            header("Location: admin.php");
+        }else if($r == null){
+            header("Location: index.php");
         }
     }
     mysqli_close($con);
 ?>
+</body>
+</html>
