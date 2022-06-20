3c23de30a31faef73e12ed49fbaf37b1463c0d82
CongTuAoDen2k5 <CongTuAoDen2k5@users.noreply.github.com>
2022-06-20 16:40:38 +0700

    Update huydonhang.php

--- coloshop-master/huydonhang.php
+++ coloshop-master/huydonhang.php
@@ -1,9 +1,12 @@
 <?php
 	include_once('ketnoi.php');
 	$id_user = $_GET['id_user'];
+	$sql = "UPDATE cart set status = 1 WHERE status = 2 and id_user = $id_user";
+	mysqli_query($con,$sql);
+	$sql = "DELETE FROM order_details WHERE status = 1 and id_user = $id_user";
+	mysqli_query($con,$sql);
 	$sql = "DELETE FROM orders WHERE status = 0 and id_user = $id_user";
 	mysqli_query($con,$sql);
-	$sql = "DELETE FROM order_details WHERE status = 0 and id_user = $id_user";
-	mysqli_query($con,$sql);
 	mysqli_close($con);
+	header("Location: cart.php");
 ?>
