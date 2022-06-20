9b606b077851874c9eb11d25e472d92ae668664f
CongTuAoDen2k5 <CongTuAoDen2k5@users.noreply.github.com>
2022-06-20 10:42:30 +0700

    Update cart_TangSLSP.php

--- coloshop-master/cart_TangSLSP.php
+++ coloshop-master/cart_TangSLSP.php
@@ -1,7 +1,7 @@
 <?php
 	include_once('ketnoi.php');
 	$id_pr = $_GET['id_pr'];
-    $sql = "UPDATE order_details set num = num + 1 where id_product = $id_pr";
+    $sql = "UPDATE cart set num = num + 1 where id_product = $id_pr";
     mysqli_query($con,$sql);
 	header("Location: cart.php");
 
