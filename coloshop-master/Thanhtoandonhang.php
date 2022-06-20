a7851ac4ac92049df46914cc87fa48586c4fd209
CongTuAoDen2k5 <CongTuAoDen2k5@users.noreply.github.com>
2022-06-20 22:58:55 +0700

    Delete Thanhtoandonhang.php

--- coloshop-master/Thanhtoandonhang.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once "ketnoi.php";
-    $sql = "Delete From order_details where status = '1'";
-    mysqli_query($con,$sql);
-    mysqli_close($con);
-    header("Location: cart.php");
-?>
