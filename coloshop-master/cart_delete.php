cd2a5ffd581f94dab8ad39e284f965c19fa7d86d
CongTuAoDen2k5 <CongTuAoDen2k5@users.noreply.github.com>
2022-06-20 10:42:33 +0700

    Update cart_delete.php

--- coloshop-master/cart_delete.php
+++ coloshop-master/cart_delete.php
@@ -1,7 +1,7 @@
 <?php
 	include_once('ketnoi.php');
 	$id_pr = $_GET['id_pr'];
-    $sql = "DELETE from order_details where id_product = $id_pr";
+    $sql = "DELETE from cart where id_product = $id_pr";
     mysqli_query($con,$sql);
     mysqli_close($con);
 	header("Location: cart.php");
