4fe0fb952f50eb02e292ed2b4d208f71eb63e049
QuengLinh <linhthotdai@gmail.com>
2022-06-20 10:17:27 +0700

    Update Addcart2.php

--- coloshop-master/Addcart2.php
+++ coloshop-master/Addcart2.php
@@ -4,6 +4,7 @@
     $sql1 = "Select * from product where id_product = '$id'";
     $rs = mysqli_query($con,$sql1);
     $row = mysqli_fetch_assoc($rs);
+    $price = $row['discount'];
 
     $email = $_COOKIE['email'];
     $sql4 = "Select id_user from user where email = '$email'";
@@ -11,25 +12,25 @@
     $row4 = mysqli_fetch_assoc($rs4);
     $id_user = $row4['id_user'];
 
-    $price = $row['price'];
-    $sql2 = "Select * from order_details where id_product = '$id'";
+    
+    $sql2 = "Select * from cart where id_product = '$id' AND id_user = '$id_user'";
     $rs2 = mysqli_query($con,$sql2);
     $row2 = mysqli_fetch_assoc($rs2);
         if($row2 != null){
             $tempnum = $row2['num']+1;
             $tempprice = ($row2['num']+1)*$price;
-            $sql = "UPDATE order_details
-            SET num = '$tempnum'
+            $sql = "UPDATE cart
+            SET num = '$tempnum', total_money = '$tempprice'
             WHERE id_product = '$id' && id_user ='$id_user'";
             mysqli_query($con,$sql);
             mysqli_close($con);
         }
         else if($row2 == null){
-            $sql3 = "INSERT INTO order_details (id_user, id_product, price, num, status)
-            VALUES ('$id_user', '$id', '$price', 1,0)";
+            $sql3 = "INSERT INTO cart (id_user, id_product, num, price, total_money ,status)
+            VALUES ('$id_user', '$id', 1, '$price', '$price',1)";
             mysqli_query($con,$sql3);
             mysqli_close($con);
         }
 
-    header("Location: cart.php");
+    //header("Location: cart.php");
 ?>
